76d7164e34596cb82b8bdb1ef68c494e29295534
Kuo <peiyukuo@fetca712057.onmicrosoft.com>
2020-07-19 15:09:38 +0800

    init

---
+++ src/main/java/com/starter/corekurondomo/CoreKurondomoApplication.java
@@ -0,0 +1,13 @@
+package com.starter.corekurondomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreKurondomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreKurondomoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starter/corekurondomo/CoreKurondomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter.corekurondomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreKurondomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
